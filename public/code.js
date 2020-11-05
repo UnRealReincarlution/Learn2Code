00408c977edfda32029c068f44fefabc3bcffeca
UnRealReincarlution <37007079+UnRealReincarlution@users.noreply.github.com>
2020-11-05 14:56:12 +1300

    Updated UI

---
+++ public/code.js
@@ -0,0 +1,22 @@
+require.config({ paths: { 'vs': 'https://unpkg.com/monaco-editor@latest/min/vs' }});
+window.MonacoEnvironment = { getWorkerUrl: () => proxy };
+
+let proxy = URL.createObjectURL(new Blob([`
+	self.MonacoEnvironment = {
+		baseUrl: 'https://unpkg.com/monaco-editor@latest/min/'
+	};
+	importScripts('https://unpkg.com/monaco-editor@latest/min/vs/base/worker/workerMain.js');
+`], { type: 'text/javascript' }));
+
+require(["vs/editor/editor.main"], function () {
+	monaco.editor.create(document.getElementById('container'), {
+		value: [
+			'int main() {',
+			'\tint number = 5;',
+			'\tchar character = \'A\';',
+			'}'
+		].join('\n'),
+		language: 'cpp',
+		theme: 'vs-dark'
+	});
+});
--- public/script.js
+++ public/script.js
@@ -1,22 +1,9 @@
-require.config({ paths: { 'vs': 'https://unpkg.com/monaco-editor@latest/min/vs' }});
-window.MonacoEnvironment = { getWorkerUrl: () => proxy };
+// IIFE
+(() => {
+    console.log("Loaded Content")
+})()
 
-let proxy = URL.createObjectURL(new Blob([`
-	self.MonacoEnvironment = {
-		baseUrl: 'https://unpkg.com/monaco-editor@latest/min/'
-	};
-	importScripts('https://unpkg.com/monaco-editor@latest/min/vs/base/worker/workerMain.js');
-`], { type: 'text/javascript' }));
-
-require(["vs/editor/editor.main"], function () {
-	monaco.editor.create(document.getElementById('container'), {
-		value: [
-			'int main() {',
-			'\tint number = 5;',
-			'\tchar character = \'A\';',
-			'}'
-		].join('\n'),
-		language: 'cpp',
-		theme: 'vs-dark'
-	});
+$("#add_course").on('click', (e) => {
+    $("#overlay").css('display', 'flex');
+    console.log("overlay shown")
 });
